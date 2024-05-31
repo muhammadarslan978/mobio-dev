0b21ecdefe12fb933aa8b1b9024c8831a3d48bb3
muhammadarslan978 <arslan@99technologies.com>
2024-06-01 01:00:34 +0500

    setup auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { DatabaseModule } from './modules/database/database.module';
 import { UserModule } from './modules/user/user.module';
 import { UtilsService } from './modules/utils/utils.service';
 import { RabbitMqModule } from './modules/rabbit-mq/rabbit-mq.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { RabbitMqModule } from './modules/rabbit-mq/rabbit-mq.module';
     DatabaseModule,
     UserModule,
     RabbitMqModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [UtilsService],
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -4,6 +4,8 @@ import {
   ValidationPipe,
   UsePipes,
   Body,
+  Get,
+  Param,
 } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { WebSignUpDto, MobileSignupDto } from './dto/user.dto';
@@ -37,5 +39,40 @@ export class UserController {
   @UsePipes(new ValidationPipe())
   async mobileSignUp(@Body() body: MobileSignupDto): Promise<IUser> {
     return this.userService.mobileSignup(body);
+  }
+
+  @Get('/verify/:JWT')
+  @ApiOperation({ summary: 'Mobio verify user api' })
+  @ApiResponse({
+    status: 200,
+    description: 'Verified',
+  })
+  @ApiResponse({ status: 400, description: 'Invalid input' })
+  @UsePipes(new ValidationPipe())
+  async verify(@Param('JWT') token: string): Promise<string> {
+    try {
+      console.log(token);
+      return 'Verified';
+    } catch (error) {
+      throw new Error('Invalid input');
+    }
+  }
+
+  @Get('/resend/:JWT')
+  @ApiOperation({ summary: 'Mobio resend verification api' })
+  @ApiResponse({
+    status: 200,
+    description:
+      'Verification link send to your email please verify your account',
+  })
+  @ApiResponse({ status: 400, description: 'Invalid input' })
+  @UsePipes(new ValidationPipe())
+  async resend(@Param('JWT') token: string): Promise<string> {
+    try {
+      console.log(token);
+      return 'Verification link send to your email please verify your account';
+    } catch (error) {
+      throw new Error('Invalid input');
+    }
   }
 }
